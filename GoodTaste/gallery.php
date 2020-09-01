3c7a5b730ec667bf36e70d5d64204b2bf29818ef
Achraf <ninjagoo147@outlook.com>
2020-09-01 02:22:53 +0100

    projetavecpresentation

---
+++ GoodTaste/gallery.php
@@ -0,0 +1,157 @@
+<!Doctype html>
+
+<html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<meta name="description" content="" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<meta name="keywords" content="" />
+
+<head>
+	
+<title>GoodTaste</title>
+
+<link rel="stylesheet" href="css/main.css" />
+
+<link rel="stylesheet" href="css/lightbox.min.css" />
+
+<script src="js/jquery.min.js" ></script>
+
+<script src="js/myscript.js"></script>
+	
+</head>
+
+<body>
+	
+<?php require "includes/header.php"; ?>
+
+<div class="parallax" onclick="remove_class()">
+	
+	<div class="parallax_head">
+		
+		
+		<h2>About Us</h2>
+		
+	</div>
+	
+	</div>
+
+<div class="abouttus2">
+
+	<center> <h1 id="abouth2us">The Good<span style="color:red;">Taste</span> Restaurant</h1> </center>
+
+	<p id="par1gr">
+	Craving some delicious Greek food? Maybe you’re in the mood for a juicy steak? No matter what kind of meal you have in mind, 
+The GoodTaste Restaurant is ready to prepare it for you.
+Since 1973, The GoodTaste Restaurant has been the go-to diner for residents of Binghamton, NY. Our diner serves breakfast all day, in addition to wholesome and flavorful dining options
+ for lunch and dinner. From burgers to salads, seafood to pastas, you’ll find all kinds of hearty meals prepared fresh at The GoodTaste Restaurant. Our diner also has a full bakery 
+with delicious baked goods and other treats, including our famous cheesecake. Sounds delicious, right?
+The GoodTaste Restaurant is open daily from 5 a.m. to 11 p.m. We’re here to serve you the best food around, whenever you’re looking for a great American restaurant in Binghamton, NY.
+	</p>
+
+</div>
+
+</div>
+
+<div class="abouttus1"  >
+
+
+<h2 id="abouth1us"><span style="color:red">Now Presenting</span><br>
+Your Next Event</h2>
+
+<div class="row">
+    <div class="col-12"  >
+    <div class="col-6"style="float:left !Important">
+	
+
+
+        <p id="par2gr" >If you’ve never been to our restaurant before, you should know that it's a large <br>
+dining room that can seat many people. With that sort of space, we have the capacity <br>
+to host a range of events and special occasions.The GoodTaste Restaurant is happy to be the <br>
+venue for your next presentation, reunion, party, luncheon or banquet. With two banquet <br>
+rooms, presentation screens, an LCD projector and a podium, The GoodTaste Restaurant can <br>
+transform a space into the perfect site for your gathering.Whether you want to have your <br>
+associates attend a training luncheon or you're planning a birthday party, trust that we <br>
+have the space, equipment and menu to accommodate any occasion. To reserve a space <br>
+for your next event, come by the restaurant or call +1 (800) 654-9647 today.<br>
+</p>
+</div>
+
+<div class="row">
+  <div class="column">
+    <img src=image/FoodPics/1.jpg  id="radd">
+    
+  </div>
+  <div class="column">
+    <img src="image/FoodPics/4.jpg"  id="radd">
+    
+  </div>
+  
+</div>
+<div class="row">
+  <div class="column">
+    <img src=image/FoodPics/4.jpg  id="radd">
+    
+  </div>
+  <div class="column">
+    <img src="image/FoodPics/7.jpg"  id="radd">
+    
+  </div>
+  
+</div>
+
+
+
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+<div class="container">
+  <div class="row">
+   <div class="col-lg-6">
+    	<div class="C">
+      <img id="radd" src="image/FoodPics/cocktail-fruits-fraise-fraiche_140725-1777.jpg" width="100%" height="100%">
+   </div> 
+</div>
+
+    <div class="col-lg-6">
+      <div class="D">
+        <h2 id="abouth2us"><span style="color:red">Stop in...</span><br>
+  For a Drink or Sweet Treat</h2>
+
+<p id="par3gr">
+If you’ve never been to our restaurant before, you should know that it's a large <br>
+dining room that can seat many people. With that sort of space, we have the capacity  <br>
+to host a range of events and special occasions.The GoodTaste Restaurant is happy to be the 
+venue for your next presentation, reunion, party, luncheon <br>or banquet. With two banquet  
+rooms, presentation screens, an LCD projector and a podium,<br> The GoodTaste Restaurant can 
+transform a space into the perfect site for your  <br>gathering.Whether you want to have your  
+associates attend a training luncheon or you're planning a <br>birthday party, trust that we 
+have the space, equipment and menu to accommodate any occasion.  <br>To reserve a space  
+for your next event, come by the restaurant or call +1 (800) 654-9647 today. 
+</p>
+     
+   </div> 
+</div>
+  </div>
+</div>
+
+</div>
+
+<?php require "includes/footerr.php"; ?>
+	
+</body>
+
+</html>
+
+<script src="js/lightbox.min.js" ></script>
